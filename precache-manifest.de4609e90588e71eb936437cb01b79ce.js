9d06620350e346946b8a721cb8809e5ae3b93acf
fed-gren <deveb22@gmail.com>
2019-03-25 16:12:10 +0900

    Updates

---
+++ precache-manifest.de4609e90588e71eb936437cb01b79ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29634a488fca373968bc",
+    "url": "/Typing-app/static/js/runtime~main.f0d7213f.js"
+  },
+  {
+    "revision": "65c957ecf125f6c96896",
+    "url": "/Typing-app/static/js/main.3435eb34.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/Typing-app/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "65c957ecf125f6c96896",
+    "url": "/Typing-app/static/css/main.de8ea8be.chunk.css"
+  },
+  {
+    "revision": "70c071883730a2339b516be984e68801",
+    "url": "/Typing-app/index.html"
+  }
+];
